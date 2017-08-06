72d968884ef53d2cf7d6628963f8dce273eaab42
Lukas Hlavacka <lukas.hlavacka@gmail.com>
2017-08-05 21:40:42 -0400

    Improving functions

--- Program.fs
+++ Program.fs
@@ -3,22 +3,22 @@
 type Cell = Alive | Dead
 type World = Cell[,]
 
-let flatten (arr: 'T[,]) =
-    arr |> Seq.cast<'T> |> Seq.toArray
+module Array2D =
+    let flatten<'T> = Seq.cast<'T> >> Seq.toArray
 
-let initRandomWorld x y = 
+let initRandomWorld x y: World = 
     let rnd = Random()
     Array2D.init x y (fun x y -> if rnd.Next(2) > 0 then Alive else Dead)
 
 let world: World = initRandomWorld 3 3
 
 /// <summary>Given the 3x3 neighbourhood determines if the center should be alive</summary>
-let isAlive (neighbors: Cell[,]) =
-    match neighbors |> flatten |> Array.toList |> List.sumBy (fun cell -> if cell = Alive then 1 else 0) with
+let isAlive (neighbors: Cell[,]): Cell =
+    match neighbors |> Array2D.flatten |> Array.toList |> List.sumBy (fun cell -> if cell = Alive then 1 else 0) with
         | 4 | 5 -> Alive
         | _ -> Dead
 
 [<EntryPoint>]
-let main argv =
+let main argv: int =
     initRandomWorld 3 3 |> isAlive |> printfn "%A"
     0 // return an integer exit code
