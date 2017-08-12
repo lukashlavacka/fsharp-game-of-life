d9187487f64e0e4aeb5c269418e4b44952670db7
Lukas Hlavacka <lukas.hlavacka@gmail.com>
2017-08-12 13:20:47 -0400

    Adding first tests

--- Program.fs
+++ Program.fs
@@ -1,67 +1,6 @@
 ﻿open System
+open GameOfLife
 
-type Cell = Alive | Dead
-type World = Cell[,]
-
-module Array2D =
-    type Mode = Zero | Donut | CylinderX | CylinderY
-    let flatten<'T> = Seq.cast<'T> >> Seq.toArray
-    let toArray<'T> (arr: 'T[,]) = Array.init (Array2D.length1 arr) (fun i -> arr.[i, *] )
-    let translateX<'T> (mode: Mode) (zero: 'T) (o: int) (arr: 'T[,]) =
-        match mode with
-            | Zero -> arr |> Array2D.mapi (fun ix iy _ ->
-                    if
-                        ix - o < 0 ||
-                        ix - o >= Array2D.length1 arr
-                    then zero
-                    else arr.[ix - o, iy]
-                )
-            | Donut -> arr |> Array2D.mapi (fun ix iy _ ->
-                    if ix - o < 0 then arr.[Array2D.length1 arr + ix - o, iy]
-                    else if ix - o >= Array2D.length1 arr then arr.[(ix - o) % Array2D.length1 arr, iy]
-                    else arr.[ix - o, iy]
-                )                      
-            | _ -> arr
-    let translateY<'T> (mode: Mode) (zero: 'T) (o: int) (arr: 'T[,]) =
-        match mode with
-            | Zero -> arr |> Array2D.mapi (fun ix iy _ ->
-                    if
-                        iy - o < 0 ||
-                        iy - o >= Array2D.length2 arr
-                    then zero
-                    else arr.[ix, iy - o]
-                )
-            | Donut -> arr |> Array2D.mapi (fun ix iy _ ->
-                    if iy - o < 0 then arr.[ix, Array2D.length2 arr + iy - o]
-                    else if iy - o >= Array2D.length2 arr then arr.[ix, (iy - o) % Array2D.length2 arr]
-                    else arr.[ix, iy - o]
-                )                      
-            | _ -> arr
-    let translate<'T> (mode: Mode) (zero: 'T) (x: int, y: int) = translateX mode zero y >> translateY mode zero x
-
-module Pretty =
-    let cell = function
-        | Alive -> "1"
-        | _ -> "0"
-    let row (pretty: bool) (r: Cell[]) = 
-        (if pretty then "|" else "") +
-        (r |> Array.map cell |> String.concat "") +        
-        (if pretty then "|" else "")
-    let world (pretty: bool) (w: World) =
-        (if pretty then (Array.fold (fun c _ -> c + "_") "_" w.[0,*]) + "_\n" else "") +
-        (w |> Array2D.toArray |> Array.map (row pretty) |> String.concat "\n") +
-        (if pretty then "\n‾" + (Array.fold (fun c _ -> c + "‾") "‾" w.[0,*]) else "")
-       
-let initRandomWorld x y: World = 
-    let rnd = Random()
-    Array2D.init x y (fun x y -> if rnd.Next(2) > 0 then Alive else Dead)
-
-/// <summary>Given the 3x3 neighbourhood determines if the center should be alive</summary>
-let isAlive (neighbors: Cell[,]): Cell =
-    match neighbors |> Array2D.flatten |> Array.toList |> List.sumBy (fun cell -> if cell = Alive then 1 else 0) with
-        | 4 | 5 -> Alive
-        | _ -> Dead
-        
 let world: World = initRandomWorld 3 3
 
 [<EntryPoint>]
--- Test.fs
+++ Test.fs
@@ -2,7 +2,45 @@ module Tests
 
 open System
 open Xunit
+open GameOfLife
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Array2D.translate does nothing`` () =
+    let input = Array2D.init 3 3 (+)
+    let output = input |> Array2D.translate Array2D.Mode.Donut 1 (0,0)
+    Assert.Equal(input, output)
+
+[<Fact>]
+let ``Array2D.translate does something`` () =
+    let input = Array2D.init 3 3 (+)
+    let output = input |> Array2D.translate Array2D.Mode.Donut 1 (1,0)
+    Assert.NotEqual(input, output)
+
+[<Fact>]
+let ``Array2D.translate adds`` () =
+    let input = Array2D.init 3 3 (+)
+    let output1 = input |> Array2D.translate Array2D.Mode.Donut 1 (2,0)
+    let output2 = input |> Array2D.translate Array2D.Mode.Donut 1 (1,0) |> Array2D.translate Array2D.Mode.Donut 1 (1,0)
+    Assert.Equal(output1, output2)
+
+[<Fact>]
+let ``Array2D.translate cancels`` () =
+    let input = Array2D.init 3 3 (+)
+    let output1 = input |> Array2D.translate Array2D.Mode.Donut 1 (1,0)
+    let output2 = output1 |> Array2D.translate Array2D.Mode.Donut 1 (-1,0)
+    Assert.Equal(input, output2)
+
+
+[<Fact>]
+let ``Array2D.translate larger than size`` () =
+    let input = Array2D.init 3 3 (+)
+    let output1 = input |> Array2D.translate Array2D.Mode.Donut 1 (1,0)
+    let output2 = input |> Array2D.translate Array2D.Mode.Donut 1 (7,0)
+    Assert.Equal(output1, output2)
+
+[<Fact>]
+let ``Array2D.translate negative larger than size`` () =
+    let input = Array2D.init 3 3 (+)
+    let output1 = input |> Array2D.translate Array2D.Mode.Donut 1 (-1,0)
+    let output2 = input |> Array2D.translate Array2D.Mode.Donut 1 (-7,0)
+    Assert.Equal(output1, output2)
